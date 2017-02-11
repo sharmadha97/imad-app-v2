394b7c832d595bc5f27db5283438b6b3a76faff0
sharmadha97 <sharmadha97@gmail.com>
2017-02-11 19:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function(req,res) {
     counter =counter+1;
-    res.send(counter,tostring());
+    res.send(counter.toString());
 });
 app.get('/article-one',function (req,res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
